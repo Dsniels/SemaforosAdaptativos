571c9034b1d2910d15267f920f533c86baf4ab5b
Daniel Salazar <dsnile.salazr@gmail.com>
2024-01-30 23:15:19 -0600

    Configuracion basica de pantallas y colores

---
+++ index.py
@@ -0,0 +1,59 @@
+import pygame
+import sys
+import random
+import threading
+import time
+
+# Inicializar Pygame
+pygame.init()
+
+# Configuración de la pantalla
+WIDTH, HEIGHT = 1400, 700
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Simulador de Tráfico")
+
+# Colores
+BLANCO = (255, 255, 255)
+AZUL = (0, 0, 255)
+GRIS = (169, 169, 169)
+
+verde = {
+    0 : 9,
+    1 : 9,
+    2 : 9,
+    3 : 9
+}
+
+rojo = 100
+
+amarillo = 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
